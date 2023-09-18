fffe6f7e9fa14822cda320adff655561015d2b93
Asier Fernandez <asierfc@gmail.com>
2023-09-18 17:07:21 +0200

    Exercices 1.1 and 1.3 done

--- index.js
+++ index.js
@@ -4,21 +4,57 @@ const getAllDonuts = async () => {
     .then(response => response.json())
 }
 
+//Functions to retrive the Most sugary donut
 const getSugaryDonut = async () => {
     try{
         const result = await getAllDonuts();
-        const donutsMaped = result.items.item.map();
-
-
-        console.log(result);
-
+        manageSugaryDonuts(result);
     }catch (error){
         console.log(error);
     }   
 }
 
+const manageSugaryDonuts = (donuts) => {
+    
+    //loops through the all donuts array saving the sugar amount of each donut svaing it in 'sugarInDonuts' variable
+    const sugarInDonuts = donuts.items.item.map( donut => {
+        //per donut collects the sugar information converting it to numbers
+        const sugarAmount = Number(donut.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars.substring(0,2));
+        return sugarAmount;
+    })
+    
+    const maxSugar = Math.max(...sugarInDonuts);
+    const theDonut = donuts.items.item.filter(donut => Number(donut.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars.substring(0,2)) === maxSugar);
+    console.log("El donut con mas azucar es: " + theDonut[0].name);
+    
+}
+
+//Functions to retrieve the ironiest donut
+const getIronyDonut = async() => {
+    try{
+        const ironDonuts = await getAllDonuts();
+        findProteinestDonut(ironDonuts);
+    }catch (error){
+        console.log(error);
+    }
+}
+
+const findProteinestDonut = (donuts) => { 
+
+    const filterProteinInDonut = donuts.items.item.map( donut =>{
+        const proteinPerDonut =  Number(donut.nutrition_facts.nutrition.proteine.substring(0,2));
+        return proteinPerDonut;
+    });
+
+    const getMaxProtienAmount = Math.max(...filterProteinInDonut);
+
+    const theProteinDonut = donuts.items.item.filter( donut => Number(donut.nutrition_facts.nutrition.proteine.substring(0,2)) === getMaxProtienAmount)
+
+    console.log("El donut con mas proteina es: " + theProteinDonut[0].name);
+}
 
 
-
-
+//Functions to retrieve the less fiber donut
 getSugaryDonut();
+getIronyDonut();
+
